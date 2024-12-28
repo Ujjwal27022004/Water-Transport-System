7582faf51593ac4e8cb922f6076f186505e64ab8
bahetiaditya <bahetiaditya347@gmail.com>
2024-12-28 15:33:25 +0530

    Added Admin files

---
+++ Water-Transport-System/src/main/java/Team/Gamma/Water_Transport_System/Repository/AdminRepository.java
@@ -0,0 +1,9 @@
+package Team.Gamma.Water_Transport_System.Repository;
+
+import Team.Gamma.Water_Transport_System.Entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+
+}
