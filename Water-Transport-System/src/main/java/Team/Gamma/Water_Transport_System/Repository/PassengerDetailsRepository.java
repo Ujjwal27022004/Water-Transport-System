2ae102bedd5e1bcc3f147e39268988c9e9145eb4
Your Name <maitreyc300903@gmail.com>
2024-12-30 18:07:22 +0530

    Passenger details Module Done

---
+++ Water-Transport-System/src/main/java/Team/Gamma/Water_Transport_System/Repository/PassengerDetailsRepository.java
@@ -0,0 +1,7 @@
+package Team.Gamma.Water_Transport_System.Repository;
+
+import Team.Gamma.Water_Transport_System.Entity.PassengerDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PassengerDetailsRepository extends JpaRepository<PassengerDetails, Long> {
+}
