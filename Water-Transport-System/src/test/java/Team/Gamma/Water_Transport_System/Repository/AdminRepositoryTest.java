38fe4b696329d46374aa613f4747269ea86c09da
shrikant <shrikantchavan9404@gmail.com>
2025-01-09 14:11:35 +0530

    Added Test case for QueryController

--- Water-Transport-System/src/test/java/Team/Gamma/Water_Transport_System/Repository/AdminRepositoryTest.java
+++ Water-Transport-System/src/test/java/Team/Gamma/Water_Transport_System/Repository/AdminRepositoryTest.java
@@ -1,12 +1,9 @@
 package Team.Gamma.Water_Transport_System.Repository;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class AdminRepositoryTest {
 
-    @Test
+
     void findOneByEmailAndPassword() {
     }
 
