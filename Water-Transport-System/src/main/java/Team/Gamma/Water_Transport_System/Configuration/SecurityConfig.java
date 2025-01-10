e4a4cd13d84564e0b63cf377b09fd4c1b7529d1f
bahetiaditya <bahetiaditya347@gmail.com>
2025-01-10 17:32:21 +0530

    updated admincontroller

--- Water-Transport-System/src/main/java/Team/Gamma/Water_Transport_System/Configuration/SecurityConfig.java
+++ Water-Transport-System/src/main/java/Team/Gamma/Water_Transport_System/Configuration/SecurityConfig.java
@@ -24,17 +24,15 @@ public class SecurityConfig {
                                 "/api/v1/user/profile",
                                 "/api/v1/user/ask",
                                 "/api/v1/shipdetails",
-                                "/admindetails",
+                                "api/admindetails/adminedit",
                                 "/api/admindetails",
                                 "/usermanagement",
-                                "/usermanagement/search",
                                 "/shipdetails/search",
-                                "/admindetails/Shipadd",
+                                "/api/admindetails/Shipadd",
                                 "/api/v1/bookings",
                                 "/api/v1/payments",
                                 "/api/v1/receipts",
-                                "/passengerDetails",
-                                "api/admindetails/Shipadd").permitAll() // Allow public access
+                                "/passengerDetails" ).permitAll() // Allow public access
                         .anyRequest().authenticated() // Secure all other endpoints
                 )
                 .httpBasic(basic -> {}); // Enable Basic Authentication
